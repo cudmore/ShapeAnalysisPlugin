052fa142c9287cd3b94a309503f662715ad98ec5
cudmore <robertcudmore@gmail.com>
2019-12-13 16:30:07 -0800

    work

--- sandbox/vispy_set_data.py
+++ sandbox/vispy_set_data.py
@@ -1,42 +1,40 @@
+import sys
 import numpy as np
-#import napari
 from PyQt5 import QtWidgets
+
+import vispy
 import vispy.plot as vp
 
 class TryToUpdatePlot(QtWidgets.QWidget):
 	def __init__(self):
 		super(TryToUpdatePlot, self).__init__()
-
-		#viewer = napari.Viewer()
-
 		self.initUI()
 
 	def initUI(self):
-		fig = vp.Fig(size=(800, 600), show=False)
-
-		# random data
-		data = np.random.rand(100, 2)
-
+		# vispy
+		data = np.random.rand(100, 2) # random data
+		fig = vp.Fig(size=(800, 600))
 		line1 = fig[0, 0].plot(data=data)
 		line2 = fig[1, 0].plot(data=data)
 
+		# PyQt (with vispy fig.native)
 		self.myHBoxLayout = QtWidgets.QHBoxLayout(self)
-
-		self.myHBoxLayout.addWidget(QtWidgets.QPushButton('A Button'))
+		self.myHBoxLayout.addWidget(QtWidgets.QPushButton('My Button'))
 		self.myHBoxLayout.addWidget(fig.native)
 
 		self.show()
 
 		# update line1 plot with new data offset by 100
-		data = np.random.rand(100, 2) + 100
+		newData = np.random.rand(100, 2) + 100
 		# this call to set_data does not update the range of x/y axis
-		line1.set_data(data)
+		# If I zoom out with mouse-wheel I can verify it is plotted
+		line1.set_data(newData)
 
 		# Is there something I can add here ???
-		#line1.update()
+		line1.update()
 		#line1.view.camera.set_range()
 
 if __name__ == '__main__':
-	#with napari.gui_qt():
-	if 1:
-		test = TryToUpdatePlot()
+	appQt = QtWidgets.QApplication(sys.argv)
+	myClasss = TryToUpdatePlot()
+	vispy.app.run() # run vispy event loop, does not return
